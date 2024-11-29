d556e5c744a153a3454dff1534c66011d67298d2
Harshit <harshit242003@hotmail.com>
2024-11-29 16:15:31 +0530

    complete magic paper

---
+++ MagicPaperTests/MagicPaperTests.swift
@@ -0,0 +1,17 @@
+//
+//  MagicPaperTests.swift
+//  MagicPaperTests
+//
+//  Created by Harshit ‎ on 11/27/24.
+//
+
+import Testing
+@testable import MagicPaper
+
+struct MagicPaperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
